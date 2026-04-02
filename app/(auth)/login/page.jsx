1020eee56c1ac0b7dc20a8451e0c938fd1fbb803
elhadjiomardiop <bebeass2005@gmail.com>
2026-04-02 01:41:10 +0000

    commite-master

--- app/(auth)/login/page.jsx
+++ app/(auth)/login/page.jsx
@@ -1,6 +1,8 @@
 import { Suspense } from 'react';
 import LoginClient from './LoginClient';
 
+export const dynamic = 'force-dynamic';
+
 export default function LoginPage({ searchParams }) {
     const nextParam = Array.isArray(searchParams?.next)
         ? searchParams.next[0]
